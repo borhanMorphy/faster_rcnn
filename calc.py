dabcddf1ee1b4b14cf4d7630124950dbe6ca6982
omer.borhan <omer.borhan@turkai.com>
2020-01-08 14:44:28 +0300

    op changed

--- calc.py
+++ calc.py
@@ -1,84 +1,77 @@
+import math
 
-class Conv():
-    def __init__(self,kernel,stride,padding,fc):
+class Op:
+    def __init__(self,kernel,stride,padding,featuremap):
+        self.kernel = kernel
         self.stride = stride
         self.padding = padding
-        self.kernel = kernel
-        self.fc = fc
-    
+        self.featuremap = featuremap
+
     def __call__(self,w,h,c):
-        return self._calc(w),self._calc(h),self.fc
-    
-    def _calc(self,d):
-        return int((d-self.kernel+2*self.padding)/self.stride + 1)
+        return self._calc(w),self._calc(h),self.featuremap
 
-class Pool():
+    def _calc(self,d):
+        return math.floor((d+2*self.padding-self.kernel)/self.stride+1)
+
+class Conv(Op):
+    pass
+
+class Pool(Op):
     def __init__(self,kernel,stride,padding):
+        self.kernel = kernel
         self.stride = stride
         self.padding = padding
-        self.kernel = kernel
-    
+
     def __call__(self,w,h,c):
         return self._calc(w),self._calc(h),c
+
+alexnet = [
+    Conv(11,4,0,96),
+    Pool(3,2,0),
+
+    Conv(5,1,2,256),
+    Pool(3,2,0),
+
+    Conv(3,1,1,384),
     
-    def _calc(self,d):
-        return int((d-self.kernel+2*self.padding)/self.stride + 1)
+    Conv(3,1,1,384),
 
-def alexnet(w,h,c):
-    ops = []
-    ops.append(Conv(11,4,0,96))
-    ops.append(Pool(3,2,0))
+    Conv(3,1,1,256)
+]
 
-    ops.append(Conv(5,1,2,256))
-    ops.append(Pool(3,2,0))
-    
-    ops.append(Conv(3,1,1,384))
-    ops.append(Conv(3,1,1,384))
-    ops.append(Conv(3,1,1,256))
-    ops.append(Pool(3,2,0))
+vgg16 = [
+    Conv(3,1,1,64),
+    Conv(3,1,1,64),
+    Pool(2,2,0),
 
-    for op in ops:
-        w,h,c = op(w,h,c)
-    
-    return w,h,c
+    Conv(3,1,1,128),
+    Conv(3,1,1,128),
+    Pool(2,2,0),
 
-def vgg16(w,h,c):    
-    ops = []
-    ops.append(Conv(3,1,1,64))
-    ops.append(Conv(3,1,1,64))
-    ops.append(Pool(2,2,0))
+    Conv(3,1,1,256),
+    Conv(3,1,1,256),
+    Conv(3,1,1,256),
+    Pool(2,2,0),
 
-    ops.append(Conv(3,1,1,128))
-    ops.append(Conv(3,1,1,128))
-    ops.append(Pool(2,2,0))
+    Conv(3,1,1,512),
+    Conv(3,1,1,512),
+    Conv(3,1,1,512),
+    Pool(2,2,0),
 
-    ops.append(Conv(3,1,1,256))
-    ops.append(Conv(3,1,1,256))
-    ops.append(Conv(3,1,1,256))
-    ops.append(Pool(2,2,0))
+    Conv(3,1,1,512),
+    Conv(3,1,1,512),
+    Conv(3,1,1,512)
+]
 
-    ops.append(Conv(3,1,1,512))
-    ops.append(Conv(3,1,1,512))
-    ops.append(Conv(3,1,1,512))
-    ops.append(Pool(2,2,0))
-
-    ops.append(Conv(3,1,1,512))
-    ops.append(Conv(3,1,1,512))
-    ops.append(Conv(3,1,1,512))
-    ops.append(Pool(2,2,0))
-
-    for op in ops:
-        w,h,c = op(w,h,c)
-    
+def inference(model,w,h,c):
+    for layer in model:
+        w,h,c = layer(w,h,c)
     return w,h,c
 
 if __name__ == '__main__':
     import sys
     w,h = sys.argv[-2:]
-    w = int(w)
-    h = int(h)
-    ws,hs,cs = alexnet(w,h,3)
-    print(ws,hs,cs)
-
-    print(h/hs)
+    w,h = int(w),int(h)
+    ws,hs,cs = inference(alexnet,w,h,3)
     print(w/ws)
+    print(h/hs)
