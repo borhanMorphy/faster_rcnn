8542f9530b960517ea5758e1b41aa9b5b99f9d47
borhanMorphy <borhano.f.42@gmail.com>
2020-01-10 07:41:57 +0300

    test added

--- inference.py
+++ inference.py
@@ -52,32 +52,38 @@ class RPN(torch.nn.Module):
             in_channels=256,out_channels=4*self.anchor_size,
             kernel_size=1,stride=1)
 
-    def forward(self,x):
+    def forward(self,x,threshold:float=None):
         x = self.conv1(x)
         cls,reg = self.conv2_cls(x),self.conv2_reg(x)
         ch,cw = cls.size()[-2:]
 
         # batch,k*2,ch,cw => batch,k,2,ch,cw => batch,k,ch,cw,2
         cls = cls.view(-1,self.anchor_size,2,ch,cw).permute(0,1,3,4,2)
-        b,a,y,x = torch.where(cls[:,:,:,:,1]>self.threshold)
         
         # batch,k*4,ch,cw => batch,k,4,ch,cw => batch,k,ch,cw,4
         reg = reg.view(-1,self.anchor_size,4,ch,cw).permute(0,1,3,4,2)
         
-        # filter preds using threshold
-        reg = reg[b,a,y,x,:]
-        cls = cls[b,a,y,x,1]
-        print(a)
-        selected_anchors = self.anchors[a]
-        print("anchors: ",selected_anchors)
-        print("anchors shape: ",selected_anchors.size())
-        print("regression result: ",reg.size())
-        print("classification result: ",cls.size())
+        # filter candidates using score threshold
+        cls,reg,selected_anchors = self.apply_threshold(cls,reg,threshold)
+
         """
         for reg,score in zip(reg,cls[:,:,1]): # for every batch, apply NMS
             bboxes = reg*
         """
         return cls,reg
+    
+    def apply_threshold(self,cls:torch.Tensor,reg:torch.Tensor,threshold:float):
+
+        if not threshold:
+            threshold = self.threshold
+        
+        b,a,y,x = torch.where(cls[:,:,:,:,1]>=threshold)
+
+        # filter preds using threshold
+        reg = reg[b,a,y,x,:]
+        cls = cls[b,a,y,x,1]
+        selected_anchors = self.anchors[a]
+        return cls,reg,selected_anchors
 
 
 if __name__ == '__main__':
