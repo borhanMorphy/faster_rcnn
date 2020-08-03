3826cdaabee52f72ac8b5967a6b7b072cc338cf7
borhanMorphy <borhano.f.42@gmail.com>
2020-08-03 15:56:33 +0300

    try

--- neuralnets/rpn.py
+++ neuralnets/rpn.py
@@ -94,7 +94,7 @@ class DetectionLayer(nn.Module):
         batched_dets:List[torch.Tensor] = []
 
         scores = torch.sigmoid(cls_logits).reshape(bs,-1)
-        print("scores: ",scores[scores<0])
+        print("scores: ",scores[scores<0],scores.shape)
         offsets = reg_deltas.detach().reshape(bs,-1,4)
 
         # convert offsets to boxes
